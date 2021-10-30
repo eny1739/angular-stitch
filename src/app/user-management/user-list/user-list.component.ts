100a810e8ca31ab8bf6765e23ce834d49f8ef411
Brandsma Siringoringo <brandsmasiringo123@gmail.com>
2021-10-30 20:39:43 +0700

    [BRANDSMA] accomplished logic getAll list in list-user management

--- src/app/user-management/user-list/user-list.component.ts
+++ src/app/user-management/user-list/user-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observer } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { User } from '../models/user-management.interface';
+import { UserTodoService } from '../services/user-todo.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[] =[]
+  subcriber!: Observer<any> 
+
+  constructor(private readonly userService: UserTodoService) { }
 
   ngOnInit(): void {
+    
+  }
+
+  getAll():void{
+    this.subcriber = {
+      next: (users) => {
+        this.users = users
+      },
+      error: console.error,
+    };
+    this.userService.getAllUser()
+    .pipe(delay(500))
+    .subscribe(this.subcriber)
   }
 
 }
