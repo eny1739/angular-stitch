84db79cccd309f0d1be6ec267f328a4b8cc0cd0f
Brandsma Siringoringo <brandsmasiringo123@gmail.com>
2021-10-30 08:48:43 +0700

    [BRANDSMA] EDIT html list-user

--- src/app/user-management/user-form/user-form.component.ts
+++ src/app/user-management/user-form/user-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AlertMessage } from '../models/alert-message-interface';
+// import { FormControl, FormGroup, Validators } from '@angular/forms';
+// import { AlertMessage } from '../models/alert-message-interface';
+
 
 @Component({
   selector: 'app-user-form',
@@ -11,17 +12,17 @@ export class UserFormComponent implements OnInit {
 
   constructor() { }
 
-  user?: User;
-  message?: AlertMessage;
+  // user?: User;
+  // message?: AlertMessage;
 
-  userForm: FormGroup = new FormGroup({
-    id: new FormControl(),
-    username: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-    password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-    fullName: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    phone: new FormControl(null, [Validators.required, Validators.minLength(11)])
-  })
+  // userForm: FormGroup = new FormGroup({
+  //   id: new FormControl(),
+  //   username: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+  //   password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+  //   fullName: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+  //   email: new FormControl(null, [Validators.required, Validators.email]),
+  //   phone: new FormControl(null, [Validators.required, Validators.minLength(11)])
+  // })
 
   ngOnInit(): void {
   }
