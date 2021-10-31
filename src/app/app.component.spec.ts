2a753fc30f1acc6fc619b6b97346e4a662691506
eny1739 <enysulistyowati336@gmail.com>
2021-10-31 13:44:14 +0700

    [ENY] create unit test for login

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task-hotel-app'`, () => {
+  it(`should have as title 'Stitch's World App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task-hotel-app');
+    expect(app.title).toEqual("Stitch's World App");
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('task-hotel-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toBeUndefined();
   });
 });
